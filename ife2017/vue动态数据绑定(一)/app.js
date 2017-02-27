b597aeabcc8314a33ba6e1d1a2d081149eca0e18
bobofangwei <bobofangwei@163.com>
2017-02-27 11:10:48 +0800

    完成vue动态数据绑定（一）

--- ife2017/vue动态数据绑定(一)/app.js
+++ ife2017/vue动态数据绑定(一)/app.js
@@ -8,15 +8,12 @@ let app2 = new Observer({
   major: 'computer'
 });
 
-let app3=new Observer({
-    sex:'male',
-    prop:app1,
-});
+ 
+ 
 
-// // 要实现的结果如下：
-// app1.data.name; // 你访问了 name
-// app1.data.age = 100;  // 你设置了 age，新的值为100
-// app2.data.university; // 你访问了 university
-// app2.data.major = 'science';  // 你设置了 major，新的值为 science
+// 要实现的结果如下：
+app1.data.name; // 你访问了 name
+app1.data.age = 100;  // 你设置了 age，新的值为100
+app2.data.university; // 你访问了 university
+app2.data.major = 'science';  // 你设置了 major，新的值为 science
 
-app3.data.prop.name;
--- ife2017/vue动态数据绑定(一)/observer.js
+++ ife2017/vue动态数据绑定(一)/observer.js
@@ -1,32 +1,33 @@
-Observer=function(data){
-    this.data=data;
+Observer = function(data) {
+    this.data = data;
     this.walk();
 };
-Observer.prototype.walk=function(){
-    for(var key in this.data){
+Observer.prototype.walk = function() {
+    for (var key in this.data) {
         //hasOwnProperty会检查属性是否存在于对象中，不会检查原型链
-        if(this.data.hasOwnProperty(key)){
-            var val=this.data[key];
-            this.convert(key,val);
-            if(Object.prototype.toString.call(val)==='[object Object]'){
-                console.log(val);
+        if (this.data.hasOwnProperty(key)) {
+            var val = this.data[key];
+            if (Object.prototype.toString.call(val) === '[object Object]') {
                 new Observer(val);
             }
+            this.convert(key, val);
+
         }
     }
 };
-Observer.prototype.convert=function(key,val){
-    Object.defineProperty(this.data,key,{
+//该方法将原对象的每一个属性改写为访问器属性
+Observer.prototype.convert = function(key, val) {
+    Object.defineProperty(this.data, key, {
         //如果设置了访问描述符（setter,getter或者两者都有）的时候，javascript会忽略他们的value与writable,取而代之关系setter和getter
-        configurable:true,
-        enumerable:true,
-        get:function(){
-            console.log('你访问了'+key);    
-            return val;               
+        configurable: true,
+        enumerable: true,
+        get: function() {
+            console.log('你访问了' + key);
+            return val;
         },
-        set:function(newValue){
-            console.log('你设置了 '+key+'，新的值为 '+newValue);
-            val=newValue;
+        set: function(newValue) {
+            console.log('你设置了 ' + key + '，新的值为 ' + newValue);
+            val = newValue;
         },
     });
 };
