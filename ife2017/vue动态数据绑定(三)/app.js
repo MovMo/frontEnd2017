6bad2df465a0a327178fd156641a8d3b79fec7df
boboLeishao <buptweibo@163.com>
2017-03-02 21:49:18 +0800

    modify vue动态数据绑定(三)

--- ife2017/vue动态数据绑定(三)/app.js
+++ ife2017/vue动态数据绑定(三)/app.js
@@ -1,1 +1,16 @@
- 
+let app2 = Object.create(Observer);
+app2.setup({
+    name: {
+        firstName: 'shaofeng',
+        lastName: 'liang'
+    },
+    age: 25
+});
+
+
+app2.$watch('name', function(newName) {
+    console.log('我的姓名发生了变化，可能是姓氏变了，也可能是名字变了。')
+});
+// app2.$watch('name', function(newName) {
+//     console.log('set', arguments)
+// }); 
--- ife2017/vue动态数据绑定(三)/observer.js
+++ ife2017/vue动态数据绑定(三)/observer.js
@@ -3,14 +3,14 @@
 var Observer = Object.create(Emitter);
 
 Observer.setup = function(data) {
+    if (!isObject(data)) {
+        return;
+    }
     this.init();
     this.data = data;
-    this.parent = null;  
+    this.parent = null;
+    this.walk();
 
-    if (isObject(data)) {
-        define(data,'$observer',this);
-        this.walk();
-    }
 };
 Observer.walk = function() {
     for (var key in this.data) {
@@ -33,13 +33,7 @@ Observer.observe = function(key, val) {
     };
 
 };
-Observer.unObserve=function(key,val){
-    //这里貌似有问题，如果val是基本值的话，无法为其定义$observer属性
-    var ob=val&&val.$observer;
-    if(ob){
-        ob.parent=null;
-    }
-};
+
 
 Observer.convert = function(key, value) {
     var self = this;
@@ -50,10 +44,13 @@ Observer.convert = function(key, value) {
             return value;
         },
         set: function(newValue) {
+            if (value ===newValue) {
+                return;
+            }
             self.notify(key, key, newValue);
             //如果重新设置的newValue是对象的话，需要对该对象加以监听
-            self.unObserve(key,value);
-            self.observe(key,newValue );
+            //self.unObserve(key, value);
+            self.observe(key, newValue);
             value = newValue;
         }
     });
@@ -63,29 +60,13 @@ Observer.notify = function(key, path, val) {
     this.emit(key, path, val);
     if (this.parent) {
         var ob = this.parent.ob,
-            key = this.parent.key;
-        path = key +Observer.pathDelimiter+ path;
+            parentKey = this.parent.key;
+        path = parentKey + Observer.pathDelimiter + path;
         //递归调用，使得事件向上传递
-        ob.notify(key, path, val);
+        ob.notify(parentKey, path, val);
     }
 }
 Observer.$watch = function(key, fn) {
     this.on(key, fn);
 };
 
-let app2 = Object.create(Observer);
-app2.setup({
-    name: {
-        firstName: 'shaofeng',
-        lastName: 'liang'
-    },
-    age: 25
-});
-
-
-app2.$watch('name', function(newName) {
-    console.log('我的姓名发生了变化，可能是姓氏变了，也可能是名字变了。')
-});
-// app2.$watch('name', function(newName) {
-//     console.log('set',arguments)
-// });
